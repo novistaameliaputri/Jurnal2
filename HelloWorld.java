6edc8c9d892c1c8e0975c725bc3ec6c5a70079d6
novistaameliaputri <71473460+novistaameliaputri@users.noreply.github.com>
2020-09-20 11:52:26 +0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld   {
-       
-     public Static void main(String[] args) {		
-         System.out.println("Nama:novista amelia putri");
-		 System.out.println("NIM:6706200140");
-		 System.out.println("Kelas:D3IF-44-02");
-    }
-}
-	 
